f656c290534e2a69256a43cdce219d8f7f4c6e13
wenzirui <wenzirui@baidu.com>
2023-05-07 19:30:15 +0800

    update
    Change-Id: If5d69d9e8a2373cc68bea6e3e01a162941876bba

--- config_ubuntu22.sh
+++ config_ubuntu22.sh
@@ -42,11 +42,11 @@ sudo chmod a+r /etc/apt/keyrings/docker.gpg
 
 echo \
 "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://mirrors.ustc.edu.cn/docker-ce/linux/ubuntu \
-"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+"$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install -y docker-ce
+sudo usermod -aG docker $LOGNAME
 
 
 # config zsh
