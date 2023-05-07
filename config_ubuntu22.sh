2907e760e6026d663e640636cc1ac8fc682c0743
hmzdtz <hmzdtz@outlook.com>
2023-05-07 18:31:08 +0800

    update

--- config_ubuntu22.sh
+++ config_ubuntu22.sh
@@ -5,21 +5,32 @@ sudo sed -i 's@//.*archive.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.
 sudo sed -i 's@//.*security.ubuntu.com@//mirrors.ustc.edu.cn@g' /etc/apt/sources.list
 sudo apt-get update && apt-get upgrade -y
 
-echo '------ install common ------'
-sudo apt-get install -y zsh nala apt-file build-essential curl default-jdk vim-gtk3 git git-lfs
-sudo snap install --classic code
 
+echo '------ install common ------'
+sudo apt-get install -y zsh apt-file software-properties-common apt-transport-https wget \
+    build-essential curl default-jdk vim-gtk3 git git-lfs python3 python-is-python3 python3-pip
+
+echo -e "[global]\nindex-url = https://pypi.tuna.tsinghua.edu.cn/simple" \
+    | sudo tee /etc/pip.conf > /dev/null
+
+sudo apt-file update
+
+
+# install repo
 echo '------ install repo ------'
 mkdir -p $HOME/bin
 curl https://mirrors.tuna.tsinghua.edu.cn/git/git-repo -o $HOME/bin/repo
 chmod +x $HOME/bin/repo
-echo "export REPO_URL='https://mirrors.tuna.tsinghua.edu.cn/git/git-repo'" >> $HOME/.bashrc
 
+
+# install tools for gnome
 echo '------ install gnome tools ------'
 sudo apt-get install -y gnome-tweaks gnome-shell-extensions gnome-shell-extension-manager
 
+
 echo '------ install qemu ------'
 sudo apt-get install -y qemu-system
+
 
 # install docker
 echo '------ install docker ------'
@@ -37,5 +48,31 @@ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install -y docker-ce
 
-# config git
-git config --add oh-my-zsh.hide-dirty 1
+
+# config zsh
+echo '------ config zsh ------'
+sudo mkdir -p /etc/zsh
+sudo touch /etc/zsh/zshrc
+sudo apt-get install -y zsh-autosuggestions zsh-syntax-highlighting
+echo 'source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh' \
+    | sudo tee -a /etc/zsh/zshrc > /dev/null
+echo 'source /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh' \
+    | sudo tee -a /etc/zsh/zshrc > /dev/null
+
+
+# config env
+# rust
+echo 'RUSTUP_UPDATE_ROOT="https://mirrors.tuna.tsinghua.edu.cn/rustup/rustup"' \
+    | sudo tee -a /etc/environment > /dev/null
+echo 'RUSTUP_DIST_SERVER="https://mirrors.tuna.tsinghua.edu.cn/rustup"' \
+    | sudo tee -a /etc/environment > /dev/null
+# git-repo
+echo 'REPO_URL="https://mirrors.tuna.tsinghua.edu.cn/git/git-repo"' \
+    | sudo tee -a /etc/environment > /dev/null
+# nodejs
+echo 'NODE_MIRROR="https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/"' \
+    | sudo tee -a /etc/environment > /dev/null
+echo 'NVM_NODEJS_ORG_MIRROR=https://mirrors.ustc.edu.cn/node/' \
+    | sudo tee -a /etc/environment > /dev/null
+echo 'FNM_NODE_DIST_MIRROR=https://mirrors.tuna.tsinghua.edu.cn/nodejs-release/' \
+    | sudo tee -a /etc/environment > /dev/null
